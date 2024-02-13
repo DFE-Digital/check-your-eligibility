2e5b3c6ed2df5256bdbc6da85137288d1b87f150
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-02-13 10:05:47 +0000

    base solution

---
+++ CheckYourEligibility.Data/Models/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CheckYourEligibility.Data.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+      //  public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ CheckYourEligibility.Services/ServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using Ardalis.GuardClauses;
+using CheckYourEligibility.Data.Models;
+using CheckYourEligibility.Services.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace CheckYourEligibility.Services
+{
+    public class ServiceTest : IServiceTest
+    {
+        private  readonly ILogger _logger;
+        private readonly IEligibilityCheckContext _db;
+
+        //public ServiceTest()
+        //{
+        //}
+
+        public ServiceTest(ILoggerFactory logger, IEligibilityCheckContext dbContext)
+        {
+            logger = Guard.Against.Null(logger);
+            _db = Guard.Against.Null(dbContext);
+
+            _logger = logger.CreateLogger("ServiceTest");
+        }
+
+        public async Task<List<Student>> OnGetAsync()
+        {
+            try
+            {
+                _logger.LogInformation("GET Pages.ContactModel called.");
+               //var x = _db.Students.FirstOrDefault();
+                var result =  await _db.Students.ToListAsync();
+                return  result;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+    }
+}
