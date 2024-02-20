a4ca54ddfe332fad4a3030a02ecfb6ab38295f1c
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-02-20 10:22:01 +0000

    Get status on guid

---
+++ CheckYourEligibility.Domain/Responses/CheckEligibilityStatusResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CheckYourEligibility.Domain.Requests
+{
+    public class CheckEligibilityStatusResponse
+    {
+        public Data Data { get; set; }
+    }
+    public class Data
+    {
+        public string Status { get; set; }
+    }
+}
