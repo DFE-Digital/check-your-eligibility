386e9236dca36ce0eaf57858986362ab61965827
Niall Hatton <niall@emhol.com>
2024-02-22 17:32:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,40 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "~> 3.0.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "patricksdemostorage"
-    storage_account_name = "patricksdemostorage"
-    container_name       = "microservice"
-    key                  = "microservice.state"
-  }
+  required_version = ">= 0.14.9"
 }
-
 provider "azurerm" {
   features {}
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup01"
+  location = "westeurope"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-test"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
 }
