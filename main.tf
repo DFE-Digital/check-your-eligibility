2d9ce05e89aed97b7712b87eea4aab12a428710f
Niall Hatton <niall@emhol.com>
2024-02-26 08:51:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,3 +43,24 @@ resource "azurerm_linux_web_app" "webapp" {
     minimum_tls_version = "1.2"
   }
 }
+
+resource "azurerm_mssql_server" "mssqlserver" {
+  name                         = "mssqlserver"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "dev"
+    service offering = "Eligibility Checking Service GOV.UK"
+    product = "Eligibility Checking Service GOV.UK"
+  }
+}
