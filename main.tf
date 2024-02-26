32725dc649b25fe52f274667b5a7680e0e01cc00
Niall Hatton <niall@emhol.com>
2024-02-26 09:18:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,23 +25,3 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
-
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                  = "webapp-test"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  https_only            = true
-  site_config { 
-    minimum_tls_version = "1.2"
-  }
-}
