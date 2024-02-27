df4bd9c107276f15fb435ee86033c3dbcd46e388
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-02-27 09:32:37 +0000

    Added Get

---
+++ CheckYourEligibility.Data.Migrations/Migrations/20240227092811_createdDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckYourEligibility.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeStamp",
+                table: "FsmCheckEligibility",
+                newName: "Created");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "FsmCheckEligibility",
+                newName: "TimeStamp");
+        }
+    }
+}
