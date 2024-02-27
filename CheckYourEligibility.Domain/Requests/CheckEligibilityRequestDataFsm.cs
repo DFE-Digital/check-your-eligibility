ddd767f1ed0aac98d5724354a61f75c2d271a608
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-02-27 10:58:14 +0000

    updated validation

--- CheckYourEligibility.Domain/Requests/CheckEligibilityRequestDataFsm.cs
+++ CheckYourEligibility.Domain/Requests/CheckEligibilityRequestDataFsm.cs
@@ -4,12 +4,12 @@ namespace CheckYourEligibility.Domain.Requests
 {
     public class CheckEligibilityRequestDataFsm
     {
-        public string NationalInsuranceNumber { get; set; } 
+        public string? NationalInsuranceNumber { get; set; } 
 
         public string LastName { get; set; }
                
         public string DateOfBirth { get; set; }
                
-        public string NationalAsylumSeekerServiceNumber { get; set; } 
+        public string? NationalAsylumSeekerServiceNumber { get; set; } 
     }
 }
