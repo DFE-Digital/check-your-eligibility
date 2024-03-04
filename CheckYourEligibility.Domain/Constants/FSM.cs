9afd8671f87fd51d717467e1d720281a5bf64bbc
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-03-04 09:29:56 +0000

    logic app settings

--- CheckYourEligibility.Domain/Constants/FSM.cs
+++ CheckYourEligibility.Domain/Constants/FSM.cs
@@ -6,6 +6,7 @@ namespace CheckYourEligibility.Domain.Constants
     {
         public const string GetLink = "Get eligibilityCheck: /freeSchoolMeals/";
         public const string ProcessLink = "Put eligibilityCheck Process: /freeSchoolMeals/processEligibilityCheck/";
+        public const string ProcessLinkRaw = "/freeSchoolMeals/processEligibilityCheck/";
         public const string Status = "status : ";
 
 
