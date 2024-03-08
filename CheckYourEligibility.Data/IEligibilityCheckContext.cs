fa9e9a053bd413ca2aacd6e48c33d2581f1f22ea
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-03-08 15:11:46 +0000

    Added import establishments

--- CheckYourEligibility.Data/IEligibilityCheckContext.cs
+++ CheckYourEligibility.Data/IEligibilityCheckContext.cs
@@ -6,6 +6,8 @@ public interface IEligibilityCheckContext
     DbSet<EligibilityCheck> FsmCheckEligibilities { get; set; }
     DbSet<FreeSchoolMealsHMRC> FreeSchoolMealsHMRC { get; set; }
     DbSet<FreeSchoolMealsHO> FreeSchoolMealsHO { get; set; }
+    DbSet<School> Schools { get; set; }
+    DbSet<LocalAuthority> LocalAuthorities { get; set; }
 
     Task<int> SaveChangesAsync();
 }
