3f2c8612371edcfbe3b495cbaae6ec8f24019a96
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-04-26 16:19:38 +0100

    Added check hash

---
+++ CheckYourEligibility.Domain/Exceptions/ProcessCheckException.cs
@@ -0,0 +1,24 @@
+﻿// Ignore Spelling: Fsm
+
+namespace CheckYourEligibility.Domain.Exceptions
+{
+
+    public class ProcessCheckException : Exception
+    {
+        public ProcessCheckException()
+        {
+        }
+
+        public ProcessCheckException(string message)
+            : base(message)
+        {
+        }
+
+        public ProcessCheckException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+
+}
