0d868ad074d9af4c933488c1c9e3f37560924108
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-05-09 16:26:09 +0100

    set status link

--- CheckYourEligibility.Domain/Requests/QueueMessageCheck.cs
+++ CheckYourEligibility.Domain/Requests/QueueMessageCheck.cs
@@ -5,6 +5,7 @@
         public string Type { get; set; }
         public string Guid { get; set; }
 
-        public string Url { get; set; }
+        public string ProcessUrl { get; set; }
+        public string SetStatusUrl { get; set; }
     }
 }
