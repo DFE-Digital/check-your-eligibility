8522e42ab4eb1d378ad6883e60be635212c6b9d8
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-07-08 16:41:00 +0100

    coverage

--- CheckYourEligibility.Domain/Responses/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CheckYourEligibility.Domain.Responses
-{
-    public class Response
-    {
-        public string Data { get; set; }
-        public string Links { get; set; }
-    }
-}
-
--- CheckYourEligibility.Services/SchoolSearchService.cs
+++ CheckYourEligibility.Services/SchoolSearchService.cs
@@ -26,8 +26,7 @@ namespace CheckYourEligibility.Services
             }
         }
 
-        [ExcludeFromCodeCoverage(Justification = "Tests seem to fail when part of a full run, work when run individually  not sure why")]
-        public async Task<IEnumerable<Domain.Responses.School>?> Search(string query)
+          public async Task<IEnumerable<Domain.Responses.School>?> Search(string query)
         {
             var results = new List<Domain.Responses.School>();
             var queryResult = new List<School>();
