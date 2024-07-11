50389106b437f4185dbd3c872affe114828c52d8
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-07-11 09:21:12 +0100

    added sequence to bulkupload

---
+++ CheckYourEligibility.Data.Migrations/Migrations/20240711081626_BulkUploadItemSequence.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckYourEligibility.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class BulkUploadItemSequence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sequence",
+                table: "EligibilityCheck",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                table: "EligibilityCheck");
+        }
+    }
+}
