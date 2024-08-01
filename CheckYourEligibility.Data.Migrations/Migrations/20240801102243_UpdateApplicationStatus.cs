59c5ee74c1d85a243b18e043fb3034fe7fb497eb
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-08-01 11:25:29 +0100

    set status

---
+++ CheckYourEligibility.Data.Migrations/Migrations/20240801102243_UpdateApplicationStatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckYourEligibility.Data.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateApplicationStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE dbo.ApplicationStatuses SET Type = 'Entitled' WHERE Type = 'Open'");
+            migrationBuilder.Sql("UPDATE dbo.ApplicationStatuses SET Type = 'SentForReview' WHERE Type = 'PendingApproval'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
