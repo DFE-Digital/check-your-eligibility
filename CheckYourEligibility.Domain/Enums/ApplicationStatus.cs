596d0bbbf2f1265e1a5fd5f05ca32d84a40cf4ec
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-08-13 12:24:46 +0100

    added extension and description

--- CheckYourEligibility.Domain/Enums/ApplicationStatus.cs
+++ CheckYourEligibility.Domain/Enums/ApplicationStatus.cs
@@ -1,14 +1,22 @@
 ﻿// Ignore Spelling: Fsm
 
+using System.ComponentModel;
+
 namespace CheckYourEligibility.Domain.Enums
 {
     public enum ApplicationStatus
     {
-        Entitled, 
+        [Description("Entitled")]
+        Entitled,
+        [Description("Receiving Entitlemen")]
         Receiving,
+        [Description("Evidence Needed")]
         EvidenceNeeded,
+        [Description("Sent for Review")]
         SentForReview,
+        [Description("Reviewed Entitled")]
         ReviewedEntitled,
+        [Description("Reviewed Not Entitled")]
         ReviewedNotEntitled,
     }
 }
