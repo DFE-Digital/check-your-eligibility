8d539f9a62dc23f9542668a49ba2180ad529d1bb
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-09-17 10:41:48 +0100

    request logging

--- CheckYourEligibility.WebApp/Middleware/RequestBodyLoggingMiddeware .cs
+++ CheckYourEligibility.WebApp/Middleware/RequestBodyLoggingMiddeware .cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.ApplicationInsights.DataContracts;
-using System.IO;
-using System.Threading.Tasks;
+﻿using Microsoft.ApplicationInsights.DataContracts;
 
 
 namespace CheckYourEligibility.WebApp.Middleware
@@ -22,27 +19,27 @@ namespace CheckYourEligibility.WebApp.Middleware
             var originalBodyStream = context.Response.Body;
 
             // Create a new memory stream to hold the response
-            using (var responseBody = new MemoryStream())
+            using (var body = new MemoryStream())
             {
-                context.Response.Body = responseBody;
+                context.Request.Body = body;
 
                 // Invoke the next middleware in the pipeline
                 await _next(context);
 
                 // Read the response body
-                context.Response.Body.Seek(0, SeekOrigin.Begin);
-                var responseText = await new StreamReader(context.Response.Body).ReadToEndAsync();
+                context.Request.Body.Seek(0, SeekOrigin.Begin);
+                var responseText = await new StreamReader(context.Request.Body).ReadToEndAsync();
 
                 // Attach the response body to telemetry
                 var telemetry = context.Features.Get<RequestTelemetry>();
                 if (telemetry != null)
                 {
-                    telemetry.Properties["ResponseBody"] = responseText;
+                    telemetry.Properties["RequestBody"] = responseText;
                 }
 
                 // Reset the stream position and copy it back to the original stream
-                context.Response.Body.Seek(0, SeekOrigin.Begin);
-                await responseBody.CopyToAsync(originalBodyStream);
+                context.Request.Body.Seek(0, SeekOrigin.Begin);
+                await body.CopyToAsync(originalBodyStream);
             }
         }
     }
--- CheckYourEligibility.WebApp/Middleware/ResponseBodyLoggingMiddeware.cs
+++ CheckYourEligibility.WebApp/Middleware/ResponseBodyLoggingMiddeware.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.ApplicationInsights.DataContracts;
-using System.IO;
-using System.Threading.Tasks;
+﻿using Microsoft.ApplicationInsights.DataContracts;
 
 
 namespace CheckYourEligibility.WebApp.Middleware
