89712f8fb422d5406cbeec811001695786df9af7
HANNON-HEALY, Conor <Conor.HANNON-HEALY@EDUCATION.GOV.UK>
2024-09-18 17:03:48 +0100

    exception logging middleware and telemetry

---
+++ CheckYourEligibility.WebApp/UserTelemetryInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace CheckYourEligibility.WebApp.Telemetry
+{
+    public class UserTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserTelemetryInitializer(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            if (httpContext?.User?.Identity?.IsAuthenticated == true)
+            {
+                telemetry.Context.User.AuthenticatedUserId = httpContext.User.Identity.Name;
+            }
+        }
+    }
+}
