c439a4d1a85c8da461027f1a68b208e7d32f7c86
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-09-20 09:26:16 +0100

    dwp

--- CheckYourEligibility.Services/DwpService.cs
+++ CheckYourEligibility.Services/DwpService.cs
@@ -21,29 +21,42 @@ namespace CheckYourEligibility.Services
     }
 
     [ExcludeFromCodeCoverage]
-    public class DwpService : IDwpService
+    public class DwpService : BaseService , IDwpService
     {
         private readonly ILogger _logger;
         private readonly HttpClient _httpClient;
-        private readonly string _moqControllerUrl;
+        private readonly string _controllerUrl;
+        private readonly IConfiguration _configuration;
+
+        private string _DWP_ApiInstigatingUserId;
+        private string _DWP_ApiPolicyId;
+        private string _DWP_ApiCorrelationId;
+        private string _DWP_ApiContext;
+        private string _DWP_AccessLevel;
 
         public DwpService(ILoggerFactory logger,HttpClient httpClient, IConfiguration configuration)
         {
             _logger = logger.CreateLogger("ServiceFsmCheckEligibility");
             _httpClient = httpClient;
-            _moqControllerUrl = configuration["DWPMoqControllerUrl"];
+            _configuration = configuration;
+            _controllerUrl = _configuration["Dwp:ApiControllerUrl"];
+            _DWP_ApiInstigatingUserId = _configuration["Dwp:ApiInstigatingUserId"];
+            _DWP_ApiPolicyId = _configuration["Dwp:ApiPolicyId"];
+            _DWP_ApiCorrelationId = _configuration["Dwp:ApiCorrelationId"];
+            _DWP_ApiContext = _configuration["Dwp:ApiContext"];
+            _DWP_AccessLevel = _configuration["Dwp:AccessLevel"];
         }
 
         public async Task<StatusCodeResult> CheckForBenefit(string guid)
         {
-            var uri = $"{_moqControllerUrl}/v2/citizens/{guid}/claims?benefitType={MogDWPValues.validUniversalBenefitType}";
+            var uri = $"{_controllerUrl}/v2/citizens/{guid}/claims?benefitType={MogDWPValues.validUniversalBenefitType}";
 
             try
             {
-                _httpClient.DefaultRequestHeaders.Add("instigating-user-id", "abcdef1234577890abcdeffghi");
-                _httpClient.DefaultRequestHeaders.Add("access-level", "1");
-                _httpClient.DefaultRequestHeaders.Add("correlation-id", "4c6a63f1-1924-4911-b45c-95dbad8b6c37");
-                _httpClient.DefaultRequestHeaders.Add("context", "abc-1-ab-x128881");
+                _httpClient.DefaultRequestHeaders.Add("instigating-user-id", _DWP_ApiInstigatingUserId);
+                _httpClient.DefaultRequestHeaders.Add("access-level", _DWP_AccessLevel);
+                _httpClient.DefaultRequestHeaders.Add("correlation-id", _DWP_ApiCorrelationId);
+                _httpClient.DefaultRequestHeaders.Add("context", _DWP_ApiContext);
 
                 var response = await _httpClient.GetAsync(uri);
                 if (response.IsSuccessStatusCode)
@@ -73,14 +86,14 @@ namespace CheckYourEligibility.Services
 
         public async Task<string?> GetCitizen(CitizenMatchRequest requestBody)
         {
-            var uri = $"{_moqControllerUrl}/v2/citizens";
+            var uri = $"{_controllerUrl}/v2/citizens";
             var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
             try
             {
-                content.Headers.Add("instigating-user-id", "abcdef1234577890abcdeffghi");
-                content.Headers.Add("policy-id", "fsm");
-                content.Headers.Add("correlation-id", "4c6a63f1-1924-4911-b45c-95dbad8b6c37");
-                content.Headers.Add("context", "abc-1-ab-x128881");
+                content.Headers.Add("instigating-user-id", _DWP_ApiInstigatingUserId);
+                content.Headers.Add("policy-id", _DWP_ApiPolicyId);
+                content.Headers.Add("correlation-id", _DWP_ApiCorrelationId);
+                content.Headers.Add("context", _DWP_ApiContext);
 
                 var response = await _httpClient.PostAsync(uri, content);
                 if (response.IsSuccessStatusCode)
@@ -94,6 +107,12 @@ namespace CheckYourEligibility.Services
                     if (response.StatusCode == HttpStatusCode.NotFound)
                     {
                         return CheckEligibilityStatus.parentNotFound.ToString();
+                    }
+                    else if(response.StatusCode == HttpStatusCode.UnprocessableEntity)
+                    {
+                        _logger.LogInformation($"DWP Duplicate matches found for {JsonConvert.SerializeObject(requestBody)}");
+                        TrackMetric($"DWP Duplicate Matches Found", 1);
+                        return CheckEligibilityStatus.DwpError.ToString();
                     }
                     else
                     {
