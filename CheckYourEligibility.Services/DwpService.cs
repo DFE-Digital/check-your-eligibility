fd4e8769ca0d8b42081c92fd7091dfc2a769ca05
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-09-24 11:56:41 +0100

    Added dwp Citizen claims validation

--- CheckYourEligibility.Services/DwpService.cs
+++ CheckYourEligibility.Services/DwpService.cs
@@ -1,6 +1,7 @@
 ﻿using CheckYourEligibility.Domain.Constants;
 using CheckYourEligibility.Domain.Enums;
 using CheckYourEligibility.Domain.Requests.DWP;
+using CheckYourEligibility.Domain.Responses;
 using CheckYourEligibility.Domain.Responses.DWP;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,13 +17,17 @@ namespace CheckYourEligibility.Services
 
     public interface IDwpService
     {
-        Task<StatusCodeResult> CheckForBenefit(string guid);
+        Task<StatusCodeResult> GetCitizenClaims(string guid, string effectiveFromDate, string effectiveToDate);
         Task<string?> GetCitizen(CitizenMatchRequest requestBody);
     }
 
     [ExcludeFromCodeCoverage]
     public class DwpService : BaseService , IDwpService
     {
+        public const string decision_entitled = "decision_entitled";
+        public const string statusInPayment = "in_payment";
+        public const string awardStatusLive = "live";
+
         private readonly ILogger _logger;
         private readonly HttpClient _httpClient;
         private readonly string _controllerUrl;
@@ -33,6 +38,11 @@ namespace CheckYourEligibility.Services
         private string _DWP_ApiCorrelationId;
         private string _DWP_ApiContext;
         private string _DWP_AccessLevel;
+        private double _DWP_UniversalCreditThreshhold_1;
+        private double _DWP_UniversalCreditThreshhold_2;
+        private double _DWP_UniversalCreditThreshhold_3;
+
+
 
         public DwpService(ILoggerFactory logger,HttpClient httpClient, IConfiguration configuration)
         {
@@ -45,11 +55,14 @@ namespace CheckYourEligibility.Services
             _DWP_ApiCorrelationId = _configuration["Dwp:ApiCorrelationId"];
             _DWP_ApiContext = _configuration["Dwp:ApiContext"];
             _DWP_AccessLevel = _configuration["Dwp:AccessLevel"];
+            double.TryParse(_configuration["Dwp:UniversalCreditThreshhold-1"], out _DWP_UniversalCreditThreshhold_1);
+            double.TryParse(_configuration["Dwp:UniversalCreditThreshhold-2"], out _DWP_UniversalCreditThreshhold_2);
+            double.TryParse(_configuration["Dwp:UniversalCreditThreshhold-3"], out _DWP_UniversalCreditThreshhold_3);
         }
 
-        public async Task<StatusCodeResult> CheckForBenefit(string guid)
+        public async Task<StatusCodeResult> GetCitizenClaims(string guid, string effectiveFromDate, string effectiveToDate )
         {
-            var uri = $"{_controllerUrl}/v2/citizens/{guid}/claims?benefitType={MogDWPValues.validUniversalBenefitType}";
+            var uri = $"{_controllerUrl}/v2/citizens/{guid}/claims?effectiveFromDate={effectiveFromDate}&effectiveToDate={effectiveToDate}";
 
             try
             {
@@ -61,7 +74,16 @@ namespace CheckYourEligibility.Services
                 var response = await _httpClient.GetAsync(uri);
                 if (response.IsSuccessStatusCode)
                 {
-                    return new OkResult();
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var claims = JsonConvert.DeserializeObject<DwpClaimsResponse>(jsonString);
+                    if (CheckBenefitEntitlement(guid, claims))
+                    {
+                        return new OkResult();
+                    }
+                    else
+                    {
+                        return new NotFoundResult();
+                    }
                 }
                 else
                 {
@@ -84,6 +106,89 @@ namespace CheckYourEligibility.Services
             }
         }
 
+
+        public bool CheckBenefitEntitlement(string citizenId, DwpClaimsResponse claims)
+        {
+            if (CheckStandardBenefitType(citizenId,claims, DwpBenefitType.employment_support_allowance_income_based))
+                return true;
+            else if (CheckStandardBenefitType(citizenId, claims, DwpBenefitType.income_support))
+                return true;
+            else if (CheckStandardBenefitType(citizenId, claims, DwpBenefitType.job_seekers_allowance_income_based))
+                return true;
+            else if (CheckStandardBenefitType(citizenId, claims, DwpBenefitType.pensions_credit))
+                return true;
+            else if (CheckUniversalCreditBenefitType(citizenId, claims))
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckUniversalCreditBenefitType(string citizenId, DwpClaimsResponse claims)
+        {
+            var benefit = claims.data.FirstOrDefault(x => x.attributes.benefitType == DwpBenefitType.universal_credit.ToString() 
+            && x.attributes.status == statusInPayment);
+            if (benefit != null)
+            {
+                var entitled = false;
+                var threshHoldUsed = 0;
+                var liveAwards = benefit.attributes.awards.Where(x=> x.status == awardStatusLive);
+                if (liveAwards != null && liveAwards.Count()>0)
+                {
+                    var takeHomePay = 0.00;
+                    threshHoldUsed = liveAwards.Count();
+                    if (threshHoldUsed == 1)
+                    {
+                       takeHomePay = liveAwards.Sum(x=>x.assessmentAttributes.takeHomePay);
+                        if (takeHomePay <= _DWP_UniversalCreditThreshhold_1) {
+                            entitled = true;
+                        }
+                    }
+                    else if (threshHoldUsed == 2)
+                    {
+                        takeHomePay = liveAwards.Sum(x => x.assessmentAttributes.takeHomePay);
+                        if (takeHomePay <= _DWP_UniversalCreditThreshhold_2)
+                        {
+                            entitled = true;
+                        }
+                    }
+                    else if (threshHoldUsed == 3)
+                    {
+                        takeHomePay = liveAwards.Sum(x => x.assessmentAttributes.takeHomePay);
+                        if (takeHomePay <= _DWP_UniversalCreditThreshhold_3)
+                        {
+                            entitled = true;
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception($"DWP CheckUniversal credit has {liveAwards.Count()} when there should only be 3.");
+                    }
+
+
+                    if (entitled)
+                    {
+                        _logger.LogInformation($"Dwp {DwpBenefitType.universal_credit} found for CitizenId:-{citizenId}");
+                        TrackMetric($"Dwp {DwpBenefitType.universal_credit} entitled", 1);
+                        return true;
+                    }
+                }                  
+            }
+            return false;
+        }
+
+        private bool CheckStandardBenefitType(string citizenId, DwpClaimsResponse claims, DwpBenefitType benefitType)
+        {
+            var benefit = claims.data.FirstOrDefault(x => x.attributes.benefitType == benefitType.ToString()
+            && x.attributes.status == decision_entitled);
+            if (benefit != null)
+            {
+                _logger.LogInformation($"Dwp {benefitType} found for CitizenId:-{citizenId}");
+                TrackMetric($"Dwp {benefitType} entitled", 1);
+                return true;
+            }
+            return false;
+        }
+
         public async Task<string?> GetCitizen(CitizenMatchRequest requestBody)
         {
             var uri = $"{_controllerUrl}/v2/citizens";
@@ -98,7 +203,7 @@ namespace CheckYourEligibility.Services
                 var response = await _httpClient.PostAsync(uri, content);
                 if (response.IsSuccessStatusCode)
                 {
-                    var responseData = JsonConvert.DeserializeObject<DwpResponse>(response.Content.ReadAsStringAsync().Result);
+                    var responseData = JsonConvert.DeserializeObject<DwpMatchResponse>(response.Content.ReadAsStringAsync().Result);
                     return responseData.Data.Id;
                 }
                 else
