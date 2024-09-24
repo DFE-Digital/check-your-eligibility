4a0863323f097b3bf2a471d1da9ceecb0b3ba84c
HANNON-HEALY, Conor <Conor.HANNON-HEALY@EDUCATION.GOV.UK>
2024-09-24 13:55:10 +0100

    exception trigger

---
+++ CheckYourEligibility.WebApp/Controllers/ExceptionTestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CheckYourEligibility.WebApp.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExceptionTestController : ControllerBase
+    {
+        // GET: api/ExceptionTest/Trigger
+        [HttpGet("Trigger")]
+        public IActionResult TriggerException()
+        {
+            // Intentionally throw an exception to test logging
+            throw new InvalidOperationException("This is a test exception for Azure Application Insights.");
+        }
+    }
+
+
+}
