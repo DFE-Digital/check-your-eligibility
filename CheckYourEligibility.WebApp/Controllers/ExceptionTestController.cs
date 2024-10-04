900a6d512350f375e0eaf20b9d7d63c6b1cdcf0f
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-10-04 12:53:00 +0100

    extend logging

--- CheckYourEligibility.WebApp/Controllers/ExceptionTestController.cs
+++ CheckYourEligibility.WebApp/Controllers/ExceptionTestController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using CheckYourEligibility.Domain.Requests;
+using CheckYourEligibility.Domain.Responses;
+using Microsoft.AspNetCore.Mvc;
 namespace CheckYourEligibility.WebApp.Controllers
 {
 
@@ -13,6 +16,32 @@ namespace CheckYourEligibility.WebApp.Controllers
         {
             // Intentionally throw an exception to test logging
             throw new InvalidOperationException("This is a test exception for Azure Application Insights.");
+        }
+
+        [HttpPost("Trigger")]
+        public IActionResult TriggerPostException([FromBody] CheckEligibilityRequest model)
+        {
+            // Intentionally throw an exception to test logging
+            throw new InvalidOperationException("This is a test Post exception for Azure Application Insights.");
+        }
+
+        [HttpPost("TriggerBadRequest")]
+        public IActionResult TriggerPostBadRequestResponse([FromBody] CheckEligibilityRequest model)
+        {
+            return BadRequest(new MessageResponse { Data = "Rest response data" });
+        }
+
+        [HttpPost("TriggerOkRequest")]
+        public IActionResult TriggerPostOkResponse([FromBody] CheckEligibilityRequest model)
+        {
+           
+            return new ObjectResult(new CheckEligibilityResponse()
+            {
+                Data = new StatusValue() { Status = "this is a good status" },
+                
+            })
+            { StatusCode = StatusCodes.Status202Accepted };
+
         }
     }
 
--- CheckYourEligibility.WebApp/Middleware/ResponseBodyLoggingMiddleware.cs
+++ CheckYourEligibility.WebApp/Middleware/ResponseBodyLoggingMiddleware.cs
@@ -15,31 +15,42 @@ namespace CheckYourEligibility.WebApp.Middleware
 
         public async Task Invoke(HttpContext context)
         {
-            var originalBodyStream = context.Response.Body;
-
-            using (var memStream = new MemoryStream())
-            {
-                context.Response.Body = memStream;
-
-                await _next(context);
-
-                context.Response.Body.Seek(0, SeekOrigin.Begin);
-                string responseBody = await new StreamReader(context.Response.Body).ReadToEndAsync();
-                context.Response.Body.Seek(0, SeekOrigin.Begin);
-
-                // Log the response body
-                _logger.LogInformation($"Response Body: {responseBody}");
-
-                // Optionally, attach to telemetry
-                var telemetry = context.Features.Get<RequestTelemetry>();
-                if (telemetry != null)
+                   var originalBodyStream = context.Response.Body;
+                try
                 {
-                    telemetry.Properties["ResponseBody"] = responseBody;
-                    telemetry.Properties["EceApi"] = "responseBody";
-                }
+                //if (context.Response.ContentLength != null)
+                //{
 
-                await memStream.CopyToAsync(originalBodyStream);
-            }
+                    using (var memStream = new MemoryStream())
+                    {
+                        context.Response.Body = memStream;
+
+                        await _next(context);
+
+                        context.Response.Body.Seek(0, SeekOrigin.Begin);
+                        string responseBody = await new StreamReader(context.Response.Body).ReadToEndAsync();
+                        context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+                        // Log the response body
+                        _logger.LogInformation($"Endpoint:{context.GetEndpoint()} Response Body: {responseBody} ");
+
+                        // Optionally, attach to telemetry
+                        var telemetry = context.Features.Get<RequestTelemetry>();
+                        if (telemetry != null)
+                        {
+                            telemetry.Name = "EceApiResponse";
+                            telemetry.Properties["ResponseBody"] = responseBody;
+                        }
+
+                        await memStream.CopyToAsync(originalBodyStream);
+                    }
+               // }
+                }
+                catch (Exception ex)
+                {
+                _logger.LogError(ex,"Response Middleware");
+                }
+            
         }
     }
 }
