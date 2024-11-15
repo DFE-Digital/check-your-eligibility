edffa912d7ce2d78d2a19c7ce7336b67b1ec1b60
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-11-15 15:46:55 +0000

    added create application

--- CheckYourEligibility.AcceptanceTests/CheckTest.cs
+++ CheckYourEligibility.AcceptanceTests/CheckTest.cs
@@ -38,6 +38,28 @@ namespace CheckYourEligibility.AcceptanceTests
         }
 
 
+        private void CleanUpCheckData(string checkId)
+        {
+            var checkitem = _api.Db.EligibilityCheck.First(h => h.EligibilityCheckId == checkId);
+            if (checkitem.Status != CheckEligibilityStatus.queuedForProcessing.ToString())
+            {
+                if (checkitem.EligibilityCheckHashId != null)
+                {
+                    var hash = _api.Db.EligibilityCheckHashes.First(h => h.EligibilityCheckHashId == checkitem.EligibilityCheckHashId);
+                    var application = _api.Db.Applications.FirstOrDefault(h => h.EligibilityCheckHashId == hash.EligibilityCheckHashId);
+                    if (application != null)
+                    {
+                        _api.Db.Applications.Remove(application);
+                    }
+                    _api.Db.EligibilityCheckHashes.Remove(hash);
+                }
+            }
+
+            _api.Db.EligibilityCheck.Remove(checkitem);
+            _api.Db.SaveChanges();
+        }
+
+
         [Test]
         [TestCase(CheckEligibilityStatus.eligible, "NN668767B")]
         [TestCase(CheckEligibilityStatus.notEligible, "PN668767B")]
@@ -58,13 +80,12 @@ namespace CheckYourEligibility.AcceptanceTests
 
             //act
             var responseCheck = await _api.ApiDataPostAsynch("/EligibilityCheck/FreeSchoolMeals", data, new CheckEligibilityResponse());
-            responseCheck.Data.Should();
             responseCheck.Data.Status.Should().Be(CheckEligibilityStatus.queuedForProcessing.ToString());
 
             Enum.TryParse(responseCheck.Data.Status, out CheckEligibilityStatus status);
             if (_api.RunLocal)
             {
-                var processRespopnse = await _api.ApiDataPutAsynch(responseCheck.Links.Put_EligibilityCheckProcess, new CheckEligibilityStatusResponse());
+               await _api.ApiDataPutAsynch(responseCheck.Links.Put_EligibilityCheckProcess, new CheckEligibilityStatusResponse());
                 if (expectedStatus == CheckEligibilityStatus.DwpError)
                 {
                     await _api.ApiDataPatchAsynch(responseCheck.Links.Get_EligibilityCheckStatus, new EligibilityStatusUpdateRequest { Data = new EligibilityCheckStatusData { Status = CheckEligibilityStatus.DwpError } }, new CheckEligibilityStatusResponse());
@@ -73,7 +94,7 @@ namespace CheckYourEligibility.AcceptanceTests
             }
             else
             {
-                var processQueue = await _api.ApiDataPostAsynch($"/EligibilityCheck/ProcessQueueMessages?queue={_api.StandardQueue}", data, new OkObjectResult(""));
+               await _api.ApiDataPostAsynch($"/EligibilityCheck/ProcessQueueMessages?queue={_api.StandardQueue}", data, new OkObjectResult(""));
             }
 
             var attempts = 0;
@@ -91,28 +112,90 @@ namespace CheckYourEligibility.AcceptanceTests
                 }
             }
             //clean up
-            var id = responseCheck.Links.Get_EligibilityCheck.Replace("/EligibilityCheck/","");
-
-            var checkitem = _api.Db.EligibilityCheck.First(h => h.EligibilityCheckId == id);
-            if (checkitem.Status != CheckEligibilityStatus.queuedForProcessing.ToString())
-            {
-                if (checkitem.EligibilityCheckHashId != null)
-                {
-                    var hash = _api.Db.EligibilityCheckHashes.First(h => h.EligibilityCheckHashId == checkitem.EligibilityCheckHashId);
-                    var application = _api.Db.Applications.FirstOrDefault(h => h.EligibilityCheckHashId == hash.EligibilityCheckHashId);
-                    if (application != null)
-                    {
-                        _api.Db.Applications.Remove(application);
-                    }
-                    _api.Db.EligibilityCheckHashes.Remove(hash);
-                }
-            }
-                        
-            _api.Db.EligibilityCheck.Remove(checkitem);
-            _api.Db.SaveChanges();
+            var checkId = responseCheck.Links.Get_EligibilityCheck.Replace("/EligibilityCheck/", "");
+            CleanUpCheckData(checkId);
 
             //assert
             status.Should().Be(expectedStatus);
         }
+
+        [Test]
+        public async Task CheckEligibility_fsm_and_createApplication()
+        {
+            //arrange
+            var data = new CheckEligibilityRequest_Fsm
+            {
+                Data = new CheckEligibilityRequestData_Fsm
+                {
+                    LastName = CreateString(30),
+                    DateOfBirth = "1990-12-15",
+                    NationalInsuranceNumber = "NN668767B",
+                }
+            };
+
+            //act
+            var responseCheck = await _api.ApiDataPostAsynch("/EligibilityCheck/FreeSchoolMeals", data, new CheckEligibilityResponse());
+            responseCheck.Data.Should();
+            responseCheck.Data.Status.Should().Be(CheckEligibilityStatus.queuedForProcessing.ToString());
+
+            Enum.TryParse(responseCheck.Data.Status, out CheckEligibilityStatus status);
+            if (_api.RunLocal)
+            {
+               await _api.ApiDataPutAsynch(responseCheck.Links.Put_EligibilityCheckProcess, new CheckEligibilityStatusResponse());
+               
+            }
+            else
+            {
+                await _api.ApiDataPostAsynch($"/EligibilityCheck/ProcessQueueMessages?queue={_api.StandardQueue}", data, new OkObjectResult(""));
+            }
+
+            var attempts = 0;
+            while (status == CheckEligibilityStatus.queuedForProcessing)
+            {
+                var responseStatus = await _api.ApiDataGetAsynch($"{responseCheck.Links.Get_EligibilityCheckStatus}", new CheckEligibilityStatusResponse());
+                Enum.TryParse(responseStatus.Data.Status, out CheckEligibilityStatus statusCheck);
+                status = statusCheck;
+
+                Thread.Sleep(1000); //wait 1 second
+                attempts++;
+                if (attempts >= 100)
+                {
+                    break;
+                }
+            }
+            
+            var checkId = responseCheck.Links.Get_EligibilityCheck.Replace("/EligibilityCheck/", "");
+
+            var establishmentId = _api.Db.Establishments.First().EstablishmentId;
+            var userid = _api.Db.Users.First().UserId;
+
+            var appData = new ApplicationRequest
+            {
+                Data = new ApplicationRequestData {
+                Type = CheckEligibilityType.FreeSchoolMeals,
+                ParentFirstName = CreateString(30),
+                ParentLastName = data.Data.LastName,
+                ParentNationalInsuranceNumber = data.Data.NationalInsuranceNumber,
+                ParentDateOfBirth = data.Data.DateOfBirth,
+                ParentEmail = CreateString(30) + "@test.com",
+                Establishment = establishmentId,
+                UserId = userid,
+                ChildFirstName = CreateString(30),
+                ChildLastName = CreateString(30),
+                ChildDateOfBirth = data.Data.DateOfBirth,
+                }
+            };
+
+            var responseApplication= await _api.ApiDataPostAsynch("/Application", appData, new ApplicationSaveItemResponse());
+
+            //assert
+            status.Should().Be(CheckEligibilityStatus.eligible);
+
+            responseApplication.Data.Status.Should().Be(ApplicationStatus.Entitled.ToString());
+
+            //clean up
+            CleanUpCheckData(checkId);
+        }
+
     }
 }
