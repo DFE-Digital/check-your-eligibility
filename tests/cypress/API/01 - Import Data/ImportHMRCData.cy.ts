29927c71a6047eeae77fc2c5b29fc245c6c85a8f
marten-wetterberg <marten.wetterberg@nyweb.nu>
2025-03-11 11:49:09 +0000

    Add tests for HMRC and HO data import

---
+++ tests/cypress/API/01 - Import Data/ImportHMRCData.cy.ts
@@ -0,0 +1,33 @@
+import { getandVerifyBearerToken } from '../../support/apiHelpers';
+import { validLoginRequestBody } from '../../support/requestBodies';
+
+describe('Testing the API', function () {
+    it('Receives valid FormData and processes the information correctly', function () {
+        // Declarations
+        const fileName = 'HMRCDataSubset.xml'; // File name including extension
+        const method = 'POST';
+        const url = Cypress.config('baseUrl') + '/importFsmHMRCData';
+        const fileType = 'text/xml'; // XML file type     
+        const expectedAnswer = '{"data":"HMRCDataSubset.xml - HMRC File Processed."}';
+
+
+        // Get file from fixtures as binary
+        cy.fixture(fileName, 'binary').then((excelBin: string) => {
+            // File in binary format gets converted to blob so it can be sent as Form data
+            const blob = Cypress.Blob.binaryStringToBlob(excelBin, fileType);
+
+            // Build up the form
+            const formData = new FormData();
+            formData.set('file', blob, fileName); // Adding a file to the form
+
+            // Get Bearer token
+            getandVerifyBearerToken('/api/Login', validLoginRequestBody).then((token: string) => {
+                // Perform the request
+                cy.form_request(method, url, formData, token, (response: XMLHttpRequest) => {
+                    expect(response.status).to.eq(200);
+                    expect(expectedAnswer).to.eq(response.response);
+                });
+            });
+        });
+    });
+});
