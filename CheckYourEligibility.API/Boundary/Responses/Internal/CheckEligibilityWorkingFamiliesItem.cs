22fb1e72764f56fb5d21d8ea1706871fe23c0b42
lnedeleva <lili.nedeleva@education.gov.uk>
2026-09-10 15:11:01 +0100

    make new flag nullable so the property does not show on the client site

--- CheckYourEligibility.API/Boundary/Responses/Internal/CheckEligibilityWorkingFamiliesItem.cs
+++ CheckYourEligibility.API/Boundary/Responses/Internal/CheckEligibilityWorkingFamiliesItem.cs
@@ -20,7 +20,7 @@ namespace CheckYourEligibility.API.Boundary.Responses.Internal
         public string GracePeriodEndDate { get; set; }
         public string EligibilityCode { get; set; }
         public string DateOfBirth { get; set; }
-        public bool ChildTooYoung { get; set; }
+        public bool? ChildTooYoung { get; set; }
     }
 
     public class TermValidity { 
